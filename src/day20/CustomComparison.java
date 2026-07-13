5038d3bf0981e5f8e1f9b6e01ff8f99a1b86201c
raja <rajaayandurai279@gmail.com>
2026-07-13 16:46:52 +0530

    this file has code totaldays

---
+++ src/day20/CustomComparison.java
@@ -0,0 +1,14 @@
+package day20;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+    public class CustomComparison {
+        public static void main(String [] args){
+            int [] arr = {1,2,3,4,5};
+//        Arrays.stream(arr).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
+    }
+
+
