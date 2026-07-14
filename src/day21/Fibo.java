551052d3c21167e9de01af801e11df68cfaf15b2
raja <rajaayandurai279@gmail.com>
2026-07-14 16:05:26 +0530

    this file has code totaldays

---
+++ src/day21/Fibo.java
@@ -0,0 +1,15 @@
+package day21;
+
+public class Fibo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print(fibo(i) + " ");
+        }
+    }
+    public static int fibo(int n){
+
+        if (n==0){return 0;}
+        else if (n==1){return 1;}
+        return fibo(n-1) + fibo( n-2);
+    }
+}
